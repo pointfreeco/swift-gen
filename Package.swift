1a3faeb7882f494aa9082f90fa17a7cad923a69b
Stephen Celis <stephen.celis@gmail.com>
2019-03-18 09:32:21 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Gen",
-    products: [
-        .library(name: "Gen", targets: ["Gen"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/yonaskolb/XcodeGen.git", from: "2.2.0"),
-    ],
-    targets: [
-        .target(name: "Gen", dependencies: []),
-        .testTarget(name: "GenTests", dependencies: ["Gen"]),
-    ]
+  name: "Gen",
+  products: [
+    .library(name: "Gen", targets: ["Gen"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/yonaskolb/XcodeGen.git", from: "2.2.0"),
+  ],
+  targets: [
+    .target(name: "Gen", dependencies: []),
+    .testTarget(name: "GenTests", dependencies: ["Gen"]),
+  ]
 )
