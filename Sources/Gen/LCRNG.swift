d2dc643101b934201039b951e5ff8556d85d3d61
Stephen Celis <stephen@stephencelis.com>
2020-08-28 14:32:19 -0400

    Just deprecate instead

---
+++ Sources/Gen/LCRNG.swift
@@ -0,0 +1,16 @@
+/// A linear congruential random number generator.
+@available(*, deprecated, message: "LCRNG has been deprecated due to instability across Swift versions. Use Xoshiro for seedable randomness, instead.")
+public struct LCRNG: RandomNumberGenerator {
+  public var seed: UInt64
+
+  @inlinable
+  public init(seed: UInt64) {
+    self.seed = seed
+  }
+
+  @inlinable
+  public mutating func next() -> UInt64 {
+    seed = 2_862_933_555_777_941_757 &* seed &+ 3_037_000_493
+    return seed
+  }
+}
